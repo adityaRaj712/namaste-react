b61ba062d7b84bca9ae7729eeeaea9a97798188c
adityaRaj712 <hiitsaditya7369@gmail.com>
2024-08-08 00:52:29 +0530

    first episode

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 
-const heading = React.createElement(
-    "h1", 
-    {id:"heading", xyz:"abc"
-    },
-    "Hello from React🚀"
-);
+const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, [
+    React.createElement("div",{id: "child1"},[
+        React.createElement("h1",{},"I am a h1 tag"),
+        React.createElement("h2",{},"I am a h2 tag")
+    ]),
+    React.createElement("div",{id: "child2"},[
+        React.createElement("h1",{},"I am a h1 tag"),
+        React.createElement("h2",{},"I am a h2 tag")
+    ])
+]);
 
 console.log(heading);
 
